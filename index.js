239f77d1ca45103020b7b0c18791b42be7efc2a5
phylis13 <phylisnjeri54@gmail.com>
2022-12-16 23:33:46 +0300

    searchbycity function added

--- index.js
+++ index.js
@@ -20,3 +20,43 @@ window.addEventListener("load",()=>{
         })
     }
 })
+function searchByCity(){
+  var place= document.getElementById('input').value;
+  var urlsearch= `http://api.openweathermap.org/data/2.5/weather?q=${place}&` + `appid=${apikey}`;
+
+  fetch(urlsearch).then((res)=>{
+      return res.json();
+  }).then((data)=>{
+      console.log(data);
+      weatherReport(data);
+  })
+  document.getElementById('input').value='';
+}
+
+function weatherReport(data){
+
+  var urlcast= `http://api.openweathermap.org/data/2.5/forecast?q=${data.name}&` + `appid=${apikey}`;
+
+  fetch(urlcast).then((res)=>{
+      return res.json();
+  }).then((forecast)=>{
+      console.log(forecast.city);
+      hourForecast(forecast);
+      dayForecast(forecast)
+
+      console.log(data);
+      document.getElementById('city').innerText= data.name + ', '+data.sys.country;
+      console.log(data.name,data.sys.country);
+  
+      console.log(Math.floor(data.main.temp-273));
+      document.getElementById('temperature').innerText= Math.floor(data.main.temp-273)+ ' °C';
+  
+      document.getElementById('clouds').innerText= data.weather[0].description;
+      console.log(data.weather[0].description)
+      
+      let icon1= data.weather[0].icon;
+      let iconurl= "http://api.openweathermap.org/img/w/"+ icon1 +".png";
+      document.getElementById('img').src=iconurl
+  })
+
+}
