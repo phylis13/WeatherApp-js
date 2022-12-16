4c74c424354d4afa2ab5e29511e44e9f8b549e29
phylis13 <phylisnjeri54@gmail.com>
2022-12-16 23:26:12 +0300

    api key fetch added

--- index.js
+++ index.js
@@ -1,52 +1,22 @@
-const api = {
-  key: "608cd618565f59767b1a70d5cab63162",
-  baseurl: "https://api.openweathermap.org/data/2.5/"
-}
-const searchbox = document.querySelector(`.search-box`);
-searchbox.addEventListener(`keypress`, setQuery);
+const apikey="608cd618565f59767b1a70d5cab63162";
+window.addEventListener("load",()=>{
+    if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition((position)=>{
+            let lon= position.coords.longitude;
+            let lat= position.coords.latitude;
+            const url= `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&` + `lon=${lon}&appid=${apikey}`;
+            
 
-function setQuery(evt) {
-  if (evt.keyCode == 13) {
-      getResults(searchbox.value);
-      console.log(searchbox.value);
-  }
-}
-
-function getResults (query) {
-  fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
-  .then(weather => {
-      return weather.json();
-  }).then(displayResults);
-      
-}
-
-function displayResults (weather) {
-  let city = document.querySelector(`.location .city`);
-  city.innerText = `${weather.name}, ${weather.sys.country}`;
-
-  let now = new date();
-  let date =  document.querySelector(`.location .daye`);
-  date.innerText = dateBuilder(now);
-
-  let temp = document.querySelector(`.current .temp`);
-  temp.innerHTML = `${Math.round(weather.main.temp)}<span>°c</span>`;
-
-  let waether_el = document.querySelector(`current .weather`);
-  waether_el.innerText = weather.weather[0].main;
-
-  let hilow =document.querySelector(`.hi-low`);
-  hilow.innerText = `${Math.round(weather.main.temp_min)}°c / ${weather.main.temp_max}°c`;
-  
-} 
-function dateBuilder (d) {
-  let months = ["January", "February", "March","April","May", "June","July", "August", "September", "October", "November","December",];
-  let days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-
-  let day = days[d.getDay()];
-  let date = d.getDate();
-  let month = months[d.getMonth()];
-  let year = d.getFullYear();
-
-  return `${day} ${date} ${month} ${year}`;
-
-}
+            fetch(url).then((res)=>{
+                return res.json();
+            }).then((data)=>{
+                console.log(data);
+                console.log(new Date().getTime())
+                var dat= new Date(data.dt)
+                console.log(dat.toLocaleString(undefined,'Asia/Kolkata'))
+                console.log(new Date().getMinutes())
+                weatherReport(data);
+            })
+        })
+    }
+})
